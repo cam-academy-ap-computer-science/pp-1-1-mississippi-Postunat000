7019254e9b9ed5bb6c7e64961efb3afa1cbbc6f0
postunat000 <postunat000@CAM-LAB-21.bgsd.k12.wa.us>
2019-09-17 07:49:12 -0700

    Learn how to double the S

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -2,8 +2,54 @@
 public class Mississippi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TheM();
+		TheI();
+		TheS();
+		TheI();
+		TheS();
+		TheI();
+		TheP();
+		TheI();
 	}
-
+	
+	public static void TheM() {
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		
+	}
+	
+	public static void TheS() {
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS");
+	}
+	
+	public static void TheP() {
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
+	
+	public static void TheI() {
+		System.out.println("IIIII");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("IIIII");
+	}
 }
